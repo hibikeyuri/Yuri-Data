8a529d5acbcf972e41d49f8f30f5a24dc42c609c
Hibikeyuri <hibikeyuri@gmail.com>
2022-06-08 06:44:41 +0800

    complete pagination(No vue.js implementation)

--- web/static/js/yuri.js
+++ web/static/js/yuri.js
@@ -32,100 +32,108 @@ $(function() {
         computed: {
         }
     });
-});
 
+    state =  {
+        'querySet': yuris_info,
+        'page': 1,
+        'rows': 20,
+        'window': 5
+    }
 
-// var  tableData = [{}, {}, {}];
-// var state = {
-//     'querySet': tableData,
+    function pagination(querySet, page, rows) {
 
-//     'page': 1,
-//     'rows': 5,
-//     'window': 5,
-// }
+        // for data presentation in the page (trimming yuri data)
+        var trimStart = (page - 1) * rows
+        var trimEnd = trimStart + rows
 
-// buildTable()
+        var trimmedData = querySet.slice(trimStart, trimEnd)
+        // calculate max ceiling pages number
+        var pages = Math.ceil(querySet.length / rows)
+        console.log('pages: ', pages)
 
-// function pagination(querySet, page, rows) {
+        return {
+            'querySet': trimmedData,
+            'pages': pages,
+        }
+    };
+    
+    // using jqeury to manipulate some tag or element
+    function buildTable() {
+        var table = $('#table-body')
+        var data = pagination(state.querySet, state.page, state.rows)
+        console.log('Data: ', data)
+        var myList = data.querySet
 
-//     var trimStart = (page - 1) * rows
-//     var trimEnd = trimStart + rows
+        for (let i=0; i<myList.length; i+=1) {
+            //Keep in mind we are using "Template Litterals to create rows"
+            var row = `
+                    <tr>
+                    <td><img class="yuri_icon" src="${myList[i].icon}"></td>
+                    <td><a href="/yuris/${myList[i].id}">${myList[i].name}</a></td>
+                    <td>${myList[i].author}</td>
+                    <td>${myList[i].publisher}</td>
+                    <td>${myList[i].carrier}</td>
+                    <td>${myList[i].ero}</td>
+                    </tr>
+                    `
+            table.append(row)
+        }
+        pageButtons(data.pages)
+    };
 
-//     var trimmedData = querySet.slice(trimStart, trimEnd)
+    function pageButtons(pages) {
+        var wrapper = document.getElementById('pagination-wrapper')
+        wrapper.innerHTML = ''
 
-//     var pages = Math.round(querySet.length / rows);
+        //可顯示頁數按鈕的最大數量之計算
+        var maxLeft = (state.page - Math.floor(state.window / 2))
+        var maxRight = (state.page + Math.floor(state.window / 2))
 
-//     return {
-//         'querySet': trimmedData,
-//         'pages': pages,
-//     }
-// }
-
-// function pageButtons(pages) {
-//     var wrapper = document.getElementById('pagination-wrapper')
-
-//     wrapper.innerHTML = ``
-// 	console.log('Pages:', pages)
-
-//     var maxLeft = (state.page - Math.floor(state.window / 2))
-//     var maxRight = (state.page + Math.floor(state.window / 2))
-
-//     if (maxLeft < 1) {
-//         maxLeft = 1
-//         maxRight = state.window
-//     }
-
-//     if (maxRight > pages) {
-//         maxLeft = pages - (state.window - 1)
+        if(maxLeft < 1) {
+            maxLeft = 1
+            maxRight = state.window
+        }
+        if(maxRight > pages) {
+            maxLeft = pages - (state.window - 1)
+            maxRight = pages
+            if(maxLeft < 1) {
+                maxLeft = 1
+            }
+        }
         
-//         if (maxLeft < 1){
-//         	maxLeft = 1
-//         }
-//         maxRight = pages
-//     }
-//     for (var page = maxLeft; page <= maxRight; page++) {
-//     	wrapper.innerHTML += `<button value=${page} class="page btn btn-sm btn-info">${page}</button>`
-//     }
+        for(let page=maxLeft; page<=maxRight; page+=1) {
+            if(page === state.page) {
+                wrapper.innerHTML += `<a href="javascript:void(0);" class="page active">${page}</a>`
+                continue  
+            }
+            wrapper.innerHTML += `<a href="javascript:void(0);" class="page">${page}</a>`
+        }
 
-//     if (state.page != 1) {
-//         wrapper.innerHTML = `<button value=${1} class="page btn btn-sm btn-info">&#171; First</button>` + wrapper.innerHTML
-//     }
+        if(state.page != 1) {
+            wrapper.innerHTML = '<a href="javascript:void(0);" class="pagefirst">Previous</a>' + wrapper.innerHTML
+        }
 
-//     if (state.page != pages) {
-//         wrapper.innerHTML += `<button value=${pages} class="page btn btn-sm btn-info">Last &#187;</button>`
-//     }
+        if(state.page != pages) {
+            wrapper.innerHTML += '<a href="javascript:void(0);" class="pagelast">Next</a>'
+        }
 
-//     $('.page').on('click', function() {
-//         $('#table-body').empty()
+        $(".page").on('click', function() {
+            $("#table-body").empty()
+            state.page = Number($(this).text())
+            buildTable()
+        })
 
-//         state.page = Number($(this).val())
+        $(".pagefirst").on('click', function() {
+            $("#table-body").empty()
+            state.page = 1
+            buildTable()
+        })
 
-//         buildTable()
-//     })
-
-// }
-
-// using jqeury to manipulate some tag or element
-
-// function buildTable() {
-//     var table = $('#table-body')
-
-//     var data = pagination(state.querySet, state.page, state.rows)
-//     var myList = data.querySet
-
-//     for (var i = 1 in myList) {
-//         //Keep in mind we are using "Template Litterals to create rows"
-//         var row = `<tr>
-//                   <td>${myList[i].rank}</td>
-//                   <td>${myList[i].first_name}</td>
-//                   <td>${myList[i].last_name}</td>
-//                   `
-//         table.append(row)
-//     }
-
-//     pageButtons(data.pages)
-// }
-
-function clear() {
-    return 0;
-}
+        $(".pagelast").on('click', function() {
+            $("#table-body").empty()
+            state.page = pages
+            buildTable()
+        })
+    }
+    buildTable()
+});
